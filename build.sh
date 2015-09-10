61b3676565020c0d987d07715b61eb8c9d2cd165
Gaston Montes <gastonmontes@hotmail.com>
2015-09-10 16:10:56 -0300

    Clean and Build script finished.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xcodebuild -workspace ObjectiveCJavascriptIntegration.xcworkspace -scheme Debug SYMROOT=$(PWD)/build
+xcodebuild -workspace ObjectiveCJavascriptIntegration.xcworkspace -scheme ObjectiveCJavascriptIntegration -configuration Debug ONLY_ACTIVE_ARCH=NO clean build SYMROOT=$(PWD)/build
+rm -r ${PWD}/build
